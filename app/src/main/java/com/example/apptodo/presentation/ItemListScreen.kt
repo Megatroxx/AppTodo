9193a8f4651f411c238cd2e4e302372114059c05
Megatroxx <mobilkaxxx@icloud.com>
2024-07-13 06:30:06 +0400

    www

--- app/src/main/java/com/example/apptodo/presentation/ItemListScreen.kt
+++ app/src/main/java/com/example/apptodo/presentation/ItemListScreen.kt
@@ -1,5 +1,6 @@
 package com.example.apptodo.presentation
 
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
--- app/src/main/java/com/example/apptodo/presentation/viewmodels/ItemListViewModel.kt
+++ app/src/main/java/com/example/apptodo/presentation/viewmodels/ItemListViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.apptodo.presentation.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.apptodo.data.entity.TodoItem
